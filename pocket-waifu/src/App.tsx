d15298594628a6629ad9a3946d8229d3cf2eac8b
Ayuzawa <pfialkowski@student.agh.edu.pl>
2023-11-27 21:45:46 +0100

    change to react project

---
+++ pocket-waifu/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div id ='app-wrapper'>
+      <h1>POCKET WAIFU</h1>
+      <img id='waifuImage' src='waifu.jpg'></img>
+    </div>
+  );
+}
+
+export default App;
