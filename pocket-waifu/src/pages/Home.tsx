9a95412983bf77200fd3701ec1dd17861b2d8dcf
Plast0 <exato2@gmail.com>
2024-01-06 16:31:28 +0100

    anime pages update

--- pocket-waifu/src/pages/Home.tsx
+++ pocket-waifu/src/pages/Home.tsx
@@ -1,7 +1,18 @@
 import React from "react";
+import background from "./waifu.jpg";
+import './Home.css';
 
 const Home = () =>{
-    return <img alt="gojo" src="../waifu.jpg" height={860}/>
+    return(
+        <div className="containerHome" style={{ 
+            height:'82vh',
+        }}>
+        <div className="divHome">
+        <h1 style={{fontSize:"64px"}}>WELCOME TO ANIME DATABASE</h1>
+        <h1>Here you can browse anime series and maybe you will find something for yourself.</h1>
+        </div>
+        </div>
+    ) 
 }
 
 export default Home;
